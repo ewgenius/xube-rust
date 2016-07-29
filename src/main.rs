fdcf940a3cd88b321aab64cf3458ca7e1cdf7af5
ewgenius <ewgeniux@gmail.com>
2016-07-29 09:41:54 +0300

    upd deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate amethyst;
+
+use amethyst::engine::{Application, Duration, State, Trans};
+
+struct Xube;
+
+impl State for Xube {
+    fn on_start(&mut self) {
+        println!("Game started!");
+    }
+
+    fn update(&mut self, _delta: Duration) -> Trans {
+        println!("Hello from Amethyst!");
+        Trans::Quit
+    }
+
+    fn on_stop(&mut self) {
+        println!("Game stopped!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Application::new(Xube);
+    game.run();
 }
